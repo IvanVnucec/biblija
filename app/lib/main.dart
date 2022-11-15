550819c101facc7ba82f23f423bd6f01ca5e6177
IvanVnucec <vnucec.ivan@gmail.com>
2022-11-15 22:03:50 +0100

    add front page with colors

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,3 +1,229 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+void main() {
+  GoogleFonts.config.allowRuntimeFetching = false;
+
+  LicenseRegistry.addLicense(() async* {
+    final license = await rootBundle.loadString('google_fonts/OFL.txt');
+    yield LicenseEntryWithLineBreaks(['google_fonts'], license);
+  });
+
+  runApp(const App());
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+          // TODO: add colors based on the theme
+          ),
+      themeMode: ThemeMode.system,
+      darkTheme: ThemeData.dark(),
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 237, 195, 103),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          const SizedBox(height: 80),
+          Column(
+            children: [
+              Text(
+                "Biblija",
+                style: GoogleFonts.unifrakturCook(
+                  fontSize: 120,
+                  color: const Color.fromRGBO(139, 26, 16, 1),
+                ),
+              ),
+              const Text(
+                "Krščanske sadašnjosti",
+                style: TextStyle(
+                  fontSize: 18,
+                  fontStyle: FontStyle.italic,
+                  color: Color.fromARGB(255, 40, 40, 40),
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(height: 80),
+          const HomePageButtons(),
+          const SizedBox(height: 50),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              HomePageSettingsButton(),
+              HomePageBookmarkButton(),
+              HomePageLastReadButton(),
+              HomePageSearchButton(),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class HomePageLogo extends StatelessWidget {
+  const HomePageLogo({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 3),
+      child: Image.asset(
+        'assets/holy_spirit.png',
+        height: 150,
+        color: Theme.of(context).primaryColorDark,
+      ),
+    );
+  }
+}
+
+class HomePageButtons extends StatelessWidget {
+  const HomePageButtons({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        StariZavjetButton(),
+        NoviZavjetButton(),
+      ],
+    );
+  }
+}
+
+class StariZavjetButton extends ZavjetButton {
+  StariZavjetButton({super.key})
+      : super(
+          heading: 'stari zavjet',
+          onPressed: () {},
+        );
+}
+
+class NoviZavjetButton extends ZavjetButton {
+  NoviZavjetButton({super.key})
+      : super(
+          heading: 'novi zavjet',
+          onPressed: () {},
+        );
+}
+
+class ZavjetButton extends StatelessWidget {
+  final String heading;
+  final VoidCallback onPressed;
+
+  const ZavjetButton({
+    super.key,
+    required this.heading,
+    required this.onPressed,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final ButtonStyle buttonStyle = ElevatedButton.styleFrom(
+      textStyle: const TextStyle(
+        fontSize: 22,
+        fontWeight: FontWeight.bold,
+      ),
+      backgroundColor: const Color.fromARGB(255, 173, 33, 20),
+      padding: const EdgeInsets.symmetric(vertical: 20),
+      minimumSize: const Size(170, 85),
+      shape: const RoundedRectangleBorder(
+        borderRadius: BorderRadius.all(Radius.circular(15.0)),
+      ),
+    );
+
+    return ElevatedButton(
+      onPressed: onPressed,
+      style: buttonStyle,
+      child: Text(heading.toUpperCase()),
+    );
+  }
+}
+
+class HomePageSettingsButton extends HomePageRoundedButton {
+  HomePageSettingsButton({super.key})
+      : super(
+          iconData: Icons.settings,
+          onPressed: () {},
+        );
+}
+
+class HomePageBookmarkButton extends HomePageRoundedButton {
+  HomePageBookmarkButton({super.key})
+      : super(
+          iconData: Icons.bookmark,
+          onPressed: () {},
+        );
+}
+
+class HomePageLastReadButton extends HomePageRoundedButton {
+  HomePageLastReadButton({super.key})
+      : super(
+          iconData: Icons.restore,
+          onPressed: () {},
+        );
+}
+
+class HomePageSearchButton extends HomePageRoundedButton {
+  HomePageSearchButton({super.key})
+      : super(
+          iconData: Icons.search,
+          onPressed: () {},
+        );
+}
+
+class HomePageRoundedButton extends StatelessWidget {
+  final IconData iconData;
+  final VoidCallback onPressed;
+
+  const HomePageRoundedButton({
+    super.key,
+    required this.iconData,
+    required this.onPressed,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final ButtonStyle buttonStyle = ElevatedButton.styleFrom(
+      padding: const EdgeInsets.symmetric(vertical: 8),
+      shape: const CircleBorder(),
+      backgroundColor: const Color.fromARGB(255, 67, 67, 67),
+    );
+
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 3),
+      child: ElevatedButton(
+        onPressed: onPressed,
+        style: buttonStyle,
+        child: Icon(
+          iconData,
+          size: 30,
+        ),
+      ),
+    );
+  }
+}
+
+
+/*
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
@@ -218,3 +444,4 @@ class CustomPageViewScrollPhysics extends ScrollPhysics {
         damping: 1.5,
       );
 }
+*/
