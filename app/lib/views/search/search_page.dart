cff7a8db676b2e7f0144f4040287e200b9755bb6
IvanVnucec <vnucec.ivan@gmail.com>
2022-11-27 15:50:07 +0100

    reformat search bar

--- app/lib/views/search/search_page.dart
+++ app/lib/views/search/search_page.dart
@@ -4,19 +4,33 @@ import 'package:material_floating_search_bar/material_floating_search_bar.dart';
 class SearchPage extends StatelessWidget {
   const SearchPage({super.key});
 
+  Widget buildFloatingSearchBar(BuildContext context) {
+    return FloatingSearchAppBar(
+      body: ListView.builder(
+        itemCount: 5,
+        itemBuilder: (_, index) => ListTile(
+          title: Text('Item $index'),
+        ),
+      ),
+      onQueryChanged: (query) {},
+      onSubmitted: (query) {},
+      alwaysOpened: true,
+      hint: 'Search...',
+      transitionDuration: const Duration(milliseconds: 300),
+      transitionCurve: Curves.easeInOut,
+      debounceDelay: const Duration(milliseconds: 500),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: FloatingSearchAppBar(
-        alwaysOpened: true,
-        body: ListView.builder(
-          itemCount: 5,
-          itemBuilder: (_, index) => ListTile(
-            title: Text('Item $index'),
-          ),
-        ),
-        onQueryChanged: (query) {},
-        onSubmitted: (query) {},
+      resizeToAvoidBottomInset: false,
+      body: Stack(
+        fit: StackFit.expand,
+        children: [
+          buildFloatingSearchBar(context),
+        ],
       ),
     );
   }
