bf59d95c50cccd53c6ae50526c33a6d5a971a582
IvanVnucec <vnucec.ivan@gmail.com>
2022-12-13 19:11:07 +0100

    add better search page layout

--- app/lib/views/search/results_list_item.dart
+++ app/lib/views/search/results_list_item.dart
@@ -13,52 +13,33 @@ class ResultsListItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 5.0),
+      padding: const EdgeInsets.only(bottom: 8.0),
       child: Row(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
           Expanded(
             flex: 3,
-            child: _SearchResult(
-              title: title,
-              content: content,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text(
+                  title,
+                  style: TextStyle(
+                    fontSize:
+                        Theme.of(context).textTheme.labelLarge?.fontSize ??
+                            16.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const SizedBox(height: 4.0),
+                Text(
+                  content,
+                  maxLines: 3,
+                  softWrap: true,
+                  overflow: TextOverflow.ellipsis,
+                ),
+              ],
             ),
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-class _SearchResult extends StatelessWidget {
-  const _SearchResult({
-    required this.title,
-    required this.content,
-  });
-
-  final String title;
-  final String content;
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.fromLTRB(5.0, 0.0, 0.0, 0.0),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          Text(
-            title,
-            style: TextStyle(
-              fontSize: Theme.of(context).textTheme.labelLarge?.fontSize ?? 16.0,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-          const Padding(padding: EdgeInsets.symmetric(vertical: 2.0)),
-          Text(
-            content,
-            maxLines: 3,
-            softWrap: true,
-            overflow: TextOverflow.ellipsis,
           ),
         ],
       ),
--- app/lib/views/search/search_page.dart
+++ app/lib/views/search/search_page.dart
@@ -1,6 +1,7 @@
 import 'package:bible/models/bible/bible.dart';
 import 'package:bible/services/bible/search_bible.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import 'results_list_item.dart';
 
@@ -15,6 +16,9 @@ class SearchPage extends StatefulWidget {
 
 class _SearchPageState extends State<SearchPage> {
   var _searchResults = <ResultsListItem>[];
+
+  final _searchController = TextEditingController();
+  final _searchFocusNode = FocusNode();
 
   Future<List<ResultsListItem>> _search(String query) {
     return widget.bible.then(
@@ -50,33 +54,75 @@ class _SearchPageState extends State<SearchPage> {
   }
 
   @override
+  void initState() {
+    super.initState();
+    _searchController.addListener(() {
+      setState(() {});
+    });
+  }
+
+  @override
+  void dispose() {
+    _searchFocusNode.dispose();
+    _searchController.dispose();
+
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: TextField(
-                onChanged: _onChanged,
-                autofocus: true,
-                decoration: const InputDecoration(
-                  border: OutlineInputBorder(),
-                  hintText: 'Search...',
-                ),
-              ),
+      body: CustomScrollView(
+        slivers: <Widget>[
+          SliverAppBar(
+            leading: IconButton(
+              icon: const Icon(Icons.arrow_back),
+              onPressed: () => Navigator.pop(context),
             ),
-            Expanded(
-              child: ListView.builder(
-                padding: const EdgeInsets.all(8),
-                itemCount: _searchResults.length,
-                itemBuilder: (context, index) {
-                  return _searchResults[index];
+            title: TextField(
+              controller: _searchController,
+              focusNode: _searchFocusNode,
+              autofocus: true,
+              keyboardType: TextInputType.text,
+              textInputAction: TextInputAction.search,
+              decoration: InputDecoration(
+                hintText: 'Search...',
+                border: InputBorder.none,
+                enabledBorder: InputBorder.none,
+                focusedBorder: InputBorder.none,
+                disabledBorder: InputBorder.none,
+                errorBorder: InputBorder.none,
+                focusedErrorBorder: InputBorder.none,
+                fillColor: Theme.of(context).appBarTheme.backgroundColor,
+              ),
+              onChanged: _onChanged,
+            ),
+            floating: false,
+            pinned: true,
+            snap: false,
+            actions: <Widget>[
+              IconButton(
+                icon: const Icon(Icons.clear),
+                onPressed: () {
+                  _searchController.clear();
+                  _searchFocusNode.requestFocus();
+                  SystemChannels.textInput.invokeMethod('TextInput.show');
                 },
               ),
+            ],
+          ),
+          SliverPadding(
+            padding: const EdgeInsets.all(8.0),
+            sliver: SliverList(
+              delegate: SliverChildBuilderDelegate(
+                (BuildContext context, int index) {
+                  return _searchResults[index];
+                },
+                childCount: _searchResults.length,
+              ),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
