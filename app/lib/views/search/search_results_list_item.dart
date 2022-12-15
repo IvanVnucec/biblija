78420029d33233a02acdbe2cdfa7efe44c3a941b
IvanVnucec <vnucec.ivan@gmail.com>
2022-12-15 22:42:31 +0100

    code reformat

--- app/lib/views/search/search_results_list_item.dart
+++ app/lib/views/search/search_results_list_item.dart
@@ -1,8 +1,6 @@
 import 'package:bible/models/bible/search_result.dart';
 import 'package:flutter/material.dart';
 
-import 'highlighted_text.dart';
-
 class SearchResultListItem extends StatelessWidget {
   const SearchResultListItem({
     super.key,
@@ -18,22 +16,59 @@ class SearchResultListItem extends StatelessWidget {
       children: <Widget>[
         Text(
           result.chapter.name,
-          style: TextStyle(
-            fontSize: Theme.of(context).textTheme.labelLarge?.fontSize ?? 16.0,
-            fontWeight: FontWeight.bold,
-          ),
+          style: Theme.of(context).textTheme.labelLarge?.copyWith(
+                fontWeight: FontWeight.bold,
+              ),
         ),
         const SizedBox(height: 4.0),
         HighlightedText(
           text: result.chapter.content,
           first: result.match.first,
           last: result.match.last,
-          maxLines: 3,
-          softWrap: true,
-          overflow: TextOverflow.ellipsis,
         ),
         const SizedBox(height: 8.0),
       ],
+    );
+  }
+}
+
+class HighlightedText extends StatelessWidget {
+  final String text;
+  final int first;
+  final int last;
+
+  const HighlightedText({
+    super.key,
+    required this.text,
+    required this.first,
+    required this.last,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Text.rich(
+      maxLines: 3,
+      softWrap: true,
+      overflow: TextOverflow.ellipsis,
+      TextSpan(
+        children: [
+          TextSpan(
+            text: text.substring(0, first),
+          ),
+          TextSpan(
+            text: text.substring(first, last),
+            style: Theme.of(context).textTheme.bodyMedium?.copyWith(
+                  backgroundColor: Theme.of(context)
+                      .textSelectionTheme
+                      .selectionColor
+                      ?.withOpacity(0.75),
+                ),
+          ),
+          TextSpan(
+            text: text.substring(last),
+          ),
+        ],
+      ),
     );
   }
 }
